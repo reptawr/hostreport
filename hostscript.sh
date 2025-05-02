f1d1ac1ff5e4425f5654299ed693ece88f95e932
reptawr <auxiliotech@gmail.com>
2025-05-02 10:34:31 -0500

    Create hostscript.sh
    add file for script

---
+++ hostscript.sh
@@ -0,0 +1,8 @@
+# disk space utilization
+df -h > /home/fount/diskspace.txt
+
+# top processes
+top | head -n 15 > /home/fount/processes.txt
+
+# memory utilization
+free > /home/fount/totalmemory.txt
